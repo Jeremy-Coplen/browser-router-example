2ed8abb35813debeb4a773f109bb08b9098fcdc6
Jeremy-Coplen <jeremycoplen.jc@gmail.com>
2021-07-28 16:59:44 -0600

    Push to home test added

--- src/Views/About/About.js
+++ src/Views/About/About.js
@@ -1,9 +1,13 @@
 import React from "react"
 
 let About = (props) => {
+    let pushToHome = () => {
+        props.history.push("/")
+    }
     return (
         <div>
             <p style={{ "color": "#FFF" }}>About</p>
+            <button style={{"marginTop": "5px"}} onClick={pushToHome}>Push to home test</button>
         </div>
     )
 }
