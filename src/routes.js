6f4f4cd8339f67cc76d185e11d11ff3d02f4f5cd
Jeremy-Coplen <jeremycoplen.jc@gmail.com>
2021-07-27 18:51:11 -0600

    Redirect function added

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Switch, Route } from "react-router-dom"
+import { Switch, Route, Redirect } from "react-router-dom"
 import Home from "./Views/Home/Home"
 import About from "./Views/About/About"
 import Legal from "./Views/Legal/Legal"
@@ -10,4 +10,5 @@ export default <Switch>
     <Route exact path="/about" component={About} />
     <Route exact path="/legal" component={Legal} />
     <Route path="/params" component={Params} />
+    <Route render={() => <Redirect to={{pathname: "/"}} />} />
 </Switch>
